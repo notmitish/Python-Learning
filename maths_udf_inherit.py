30c3f880e178d98762c0e91e7e5669d5a9672865
NotAshish <84949223+N0tAshish@users.noreply.github.com>
2024-05-25 21:23:13 +0545

    maths_udf_inherit.py
    inheritance using python functions

---
+++ maths_udf_inherit.py
@@ -0,0 +1,19 @@
+class maths:
+    def add(self,a,b):
+        print(a+b)
+    
+    def sub(self,a,b):
+        print(a-b)
+    
+    def mul(self,a,b):
+        print(a*b)
+    
+    def div(self,a,b):
+        print(a/b)
+
+
+p = maths()
+p.add(10,7)
+p.sub(10,7)
+p.mul(10,7)
+p.div(10,7)
