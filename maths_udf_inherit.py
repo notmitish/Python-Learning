30aa7821f9f41e15d6535dd50de86a4dea24eb3c
NotAshish <84949223+N0tAshish@users.noreply.github.com>
2024-05-27 21:00:57 +0545

    maths_udf_inherit.py

--- maths_udf_inherit.py
+++ maths_udf_inherit.py
@@ -1,19 +1,30 @@
 class maths:
-    def add(self,a,b):
-        print(a+b)
+    def add(self,*args):
+        s = 0
+        for a in args:
+            s = s+a
+        print(s) 
+    def sub(self,*args):
+        s = 0
+        for a in args:
+            s = s-a
+        print(s)
     
-    def sub(self,a,b):
-        print(a-b)
+    def mul(self,*args):
+        s = 1
+        for a in args:
+            s = s*a 
+        print(s)
     
-    def mul(self,a,b):
-        print(a*b)
-    
-    def div(self,a,b):
-        print(a/b)
+    def div(self,*args):
+        s = 1
+        for a in args:
+            s = s/a
+        print(s)
 
 
 p = maths()
-p.add(10,7)
-p.sub(10,7)
+p.add(10,7,1)
+p.sub(10,7,435)
 p.mul(10,7)
-p.div(10,7)
+p.div(10,7,345,65)
